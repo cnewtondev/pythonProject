61e1b75e7b64d9a83e0c552a907e0c225503b1b6
Colton Newton <coltonnewton89@gmail.com>
2021-04-19 21:41:06 -0500

    dictionary_terminal app complete

--- dictionary.py
+++ dictionary.py
@@ -7,8 +7,14 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
-    elif: len(get_close_matches(w, data.keys())) > 0:
-        return input("Did you mean %s instead? [Y=yes,N=no]: " % get_close_matches(w, data.keys)[0])
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? [Y=yes,N=no]: " % get_close_matches(w, data.keys)[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys)[0]]
+        elif yn == "N":
+            return "Sorry, That word doesn't exist, please double check your entry."
+        else: 
+            return "I don't understand your input. Please try a different word."
     else: 
         return "Sorry. That word doesn't exist. Please try a different word or try again."
 
